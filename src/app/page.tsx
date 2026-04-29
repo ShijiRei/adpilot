0c2aaf3ef31685ef0f1cd013a3b43b1139813734
ShijiRei <ulzoldic98@gmail.com>
2026-04-29 14:40:09 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { useState } from "react";
